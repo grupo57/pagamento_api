2f4afe8a7644c37617cb4f2455cefb3eeb0cef8d
marcelloaxlima <marcello_a_lima@yahoo.com>
2024-12-03 18:56:05 -0300

    Security issues

--- src/main/java/br/com/fiap/soat07/clean/infra/repository/mongo/config/DatabaseConfig.java
+++ src/main/java/br/com/fiap/soat07/clean/infra/repository/mongo/config/DatabaseConfig.java
@@ -1,5 +1,6 @@
 package br.com.fiap.soat07.clean.infra.repository.mongo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -9,10 +10,17 @@ import com.mongodb.client.MongoClients;
 
 @Configuration
 public class DatabaseConfig {
+	
+	@Value("${spring.data.mongodb.username}")
+	private String username;
+	
+	@Value("${spring.data.mongodb.password}")
+	
+	private String password;
 
 	@Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://techchallenge:techchallenge@mongodb:27017/local?authSource=admin");
+        return MongoClients.create(String.format("mongodb://%s:%s@mongodb:27017/local?authSource=admin",username, password));
     }
 
     @Bean
